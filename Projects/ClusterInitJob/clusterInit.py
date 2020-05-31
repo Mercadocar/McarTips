126598e35eae14d67292ee6a22bd8ef463af58ee
rodoflho <rodoflho@gmail.com>
2020-05-31 12:51:46 -0300

    improving cluster init py code

--- Projects/ClusterInitJob/clusterInit.py
+++ Projects/ClusterInitJob/clusterInit.py
@@ -7,59 +7,68 @@ from psycopg2.extensions import ISOLATION_LEVEL_AUTOCOMMIT
 
 # Script to create cluster required resources:
 
-# Defining Variables
 
-postgres_username = 'postgres'
-postgres_psw = 'postgres'
-postgres_host = os.environ['POC_TELEPRECO_POSTGRESQL_SERVICE_HOST']
-rabbitmq_host = os.environ['POC_TELEPRECO_RABBITMQ_HA_SERVICE_HOST']
-db_1_name = 'atendimento'
-db_2_name = 'orcamento'
+try:  
+    rabbitmq_host = os.environ['POC_TELEPRECO_RABBITMQ_HA_SERVICE_HOST']
 
-# Create required RabbitMQ Queues and Exchanges.
+    # Create required RabbitMQ Queues and Exchanges.
+    connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host))
+    channel = connection.channel()
 
-connection = pika.BlockingConnection(pika.ConnectionParameters(rabbitmq_host))
-channel = connection.channel()
+    channel.exchange_declare(exchange='Pessoa', exchange_type='direct')
+    channel.queue_declare(queue='PessoaAtualizadaEvent', durable=True)
+    channel.queue_bind(exchange='Pessoa', queue='PessoaAtualizadaEvent', routing_key='')
 
-channel.exchange_declare(exchange='Pessoa', exchange_type='direct')
-channel.queue_declare(queue='PessoaAtualizadaEvent', durable=True)
-channel.queue_bind(exchange='Pessoa', queue='PessoaAtualizadaEvent', routing_key='')
+    channel.exchange_declare(exchange='Orcamento', exchange_type='fanout')
+    channel.queue_declare(queue='OrcamentoGeradoEvent', durable=True)
+    channel.queue_bind(exchange='Orcamento', queue='OrcamentoGeradoEvent', routing_key='')
 
-channel.exchange_declare(exchange='Orcamento', exchange_type='fanout')
-channel.queue_declare(queue='OrcamentoGeradoEvent', durable=True)
-channel.queue_bind(exchange='Orcamento', queue='OrcamentoGeradoEvent', routing_key='')
+    channel.exchange_declare(exchange='PessoaCriada', exchange_type='fanout')
+    channel.queue_declare(queue='PessoaCriadaEvent', durable=True)
+    channel.queue_bind(exchange='PessoaCriada', queue='PessoaCriadaEvent', routing_key='')
 
-channel.exchange_declare(exchange='PessoaCriada', exchange_type='fanout')
-channel.queue_declare(queue='PessoaCriadaEvent', durable=True)
-channel.queue_bind(exchange='PessoaCriada', queue='PessoaCriadaEvent', routing_key='')
+    connection.close()
+except KeyError: 
+    print ("RabbitMQ not found, proceeding...")
 
-connection.close()
+try:  
+    postgres_host = os.environ['POC_TELEPRECO_POSTGRESQL_SERVICE_HOST']
+    postgres_username = 'postgres'
+    postgres_psw = 'postgres'
+    db_1_name = 'atendimento'
+    db_2_name = 'orcamento'
 
-# Create required Postgres Databases.
+    # Create required Postgres Databases.
+    con = psycopg2.connect(dbname='postgres',
+        user=postgres_username, host=postgres_host,
+        password=postgres_psw)
 
-con = psycopg2.connect(dbname='postgres',
-    user=postgres_username, host=postgres_host,
-    password=postgres_psw)
+    con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
+    cur = con.cursor()
 
-con.set_isolation_level(ISOLATION_LEVEL_AUTOCOMMIT)
-cur = con.cursor()
+    cur.execute("SELECT datname FROM pg_database;")
 
-cur.execute("SELECT datname FROM pg_database;")
+    list_database = cur.fetchall()
 
-list_database = cur.fetchall()
+    if (db_1_name,) in list_database:
+        print("'{}' Database already exist".format(db_1_name))
+    else:
+        print("'{}' Database not exist...Creating..".format(db_1_name))
+        cur.execute(sql.SQL("CREATE DATABASE {}").format(
+            sql.Identifier(db_1_name))
+        )
 
-if (db_1_name,) in list_database:
-    print("'{}' Database already exist".format(db_1_name))
-else:
-    print("'{}' Database not exist...Creating..".format(db_1_name))
-    cur.execute(sql.SQL("CREATE DATABASE {}").format(
-        sql.Identifier(db_1_name))
-    )
+    if (db_2_name,) in list_database:
+        print("'{}' Database already exist".format(db_2_name))
+    else:
+        print("'{}' Database not exist...Creating..".format(db_2_name))
+        cur.execute(sql.SQL("CREATE DATABASE {}").format(
+            sql.Identifier(db_2_name))
+        )
+except KeyError: 
+    print ("Postgres not found, proceeding...")
 
-if (db_2_name,) in list_database:
-    print("'{}' Database already exist".format(db_2_name))
-else:
-    print("'{}' Database not exist...Creating..".format(db_2_name))
-    cur.execute(sql.SQL("CREATE DATABASE {}").format(
-        sql.Identifier(db_2_name))
-    )
+
+
+
+
